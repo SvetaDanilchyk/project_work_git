639fd45030781268f232125ad74bafc97eb4c403
Svetlana <danilchiksvs@gmail.com>
2023-01-31 15:34:19 +0300

    task1 films work

--- js/scritp.js
+++ js/scritp.js
@@ -17,8 +17,9 @@
     }
 Проверить, чтобы все работало без ошибок в консоли */
 
+'use strict';
 
-const numberOfFilms = +promt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -28,13 +29,12 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = promt('Один из последних просмотренных фильмов?', ''),
-      b = +promt('На сколько оцените его?', ''),
-      c = promt('Один из последних просмотренных фильмов?', ''),
-      d = +prcmt('На сколько оцените его?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
-        
