859d9daacf097f37bb8221f1a97b2772d91be561
Svetlana <danilchiksvs@gmail.com>
2023-02-20 12:05:55 +0300

    add metod trim for sring a

--- js/scritp.js
+++ js/scritp.js
@@ -103,7 +103,7 @@ writeYourGenres();
 
 function rememberMyFilms () {
     for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
+        const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
              b = prompt('На сколько оцените его?', '');
             if (a != '' && b != '' && a != null && b != null &&  a.length < 50) {
                 personalMovieDB.movies[a] = b;
